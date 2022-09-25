40a4331fdfb52473daaa9380c877c28a717d5ac5
zjac082@live.rhul.ac.uk <83787341+XLmT09@users.noreply.github.com>
2022-09-25 08:54:59 +0100

    hosting website

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
@@ -12,10 +14,13 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://x:xxxx@cluster0.rifbygx.mongodb.net/?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Hello API!');
+});
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewURLParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewURLParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log('Running on server port: ' + PORT)))
     .catch((error) => console.log(error.message));
 
